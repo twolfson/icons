633d466bdf10c5eea256ab293ba27a614c2fa2b0
minevala <mnev@dhcp60.sulake.com>
2015-02-17 15:21:21 +0200

    Update

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,12 +2,12 @@ var gulp = require('gulp');
 var spritesmith = require('gulp.spritesmith');
 
 gulp.task('sprite', function () {
-  var spriteData = gulp.src('images/*.png').pipe(spritesmith({
+  var spriteData = gulp.src('png/*.png').pipe(spritesmith({
     imgName: 'sprite.png',
     cssName: 'sprite.css',
     algorithm: 'binary-tree',
     padding: 2
 
   }));
-  spriteData.pipe(gulp.dest('path/to/output/'));
+  spriteData.pipe(gulp.dest('.'));
 });
